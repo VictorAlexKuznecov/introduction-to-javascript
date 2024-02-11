a9daf949d42cbcfe1be5c06557b34cf8c29f374c
Victor <victoralexkuznecov@gmail.com>
2024-02-11 16:21:39 +0300

    add task 3

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ let x = 20;
 let y = 58;
 let z = 42;
 console.log(x + y + z);
+
+// Task 3
+// description:
+// Создайте две переменные. Поместите в них переменную count и превратите в
+// строку, а userName наоборот в число. Попробуйте реализовать задачу двумя
+// разными способами.
+
+let count = 42;
+let userName = "42";
+console.log(typeof String(count));
+console.log(typeof count.toString());
+console.log(typeof ("" + count));
+console.log(typeof `${count}`);
+
+console.log(typeof +userName);
+console.log(typeof Number(userName));
+console.log(typeof (userName * 1));
+console.log(typeof (userName / 1));
+console.log(typeof parseInt(userName, 10));
