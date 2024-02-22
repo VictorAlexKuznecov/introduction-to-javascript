9f81d646f51e523a07db0e250c82ae0243f110de
Victor <victoralexkuznecov@gmail.com>
2024-02-22 09:29:19 +0300

    task 2

--- main.js
+++ main.js
@@ -10,3 +10,10 @@ console.log(typeof bool);
 console.log(typeof numb);
 console.log(typeof notDefined);
 console.log(typeof nullValue);
+
+// Даны две переменные:
+// let height = 15
+// let width = 20
+// С помощью условий выведите в консоль разработчика наибольшее число.
+const findMax = (height, width) => (height > width ? height : width);
+console.log(findMax(15, 20));
