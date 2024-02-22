6fba51b315ca09c751150483b322a08b3c00d153
Victor <victoralexkuznecov@gmail.com>
2024-02-22 09:06:29 +0300

    task 1

---
+++ main.js
@@ -0,0 +1,12 @@
+// Задача 1.
+// Создайте переменные и присвойте им значения.
+let str = "true";
+let bool = false;
+let numb = 17;
+let notDefined = undefined;
+let nullValue = null;
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof numb);
+console.log(typeof notDefined);
+console.log(typeof nullValue);
