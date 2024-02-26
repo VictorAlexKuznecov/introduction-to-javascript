fc4935d56416cf9707c8551f87b773a67301a69c
Victor <victoralexkuznecov@gmail.com>
2024-02-26 09:57:49 +0300

    fix: delete comments

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* // Задача 1.
+// Задача 1.
 // Написать функцию getSum , которая будет высчитывать сумму чисел от нуля до
 // переданного в нее параметра. Если передадим число 100 то, надо вычислить
 // сумму чисел от 0 до 100 (должно получится 5050).
@@ -6,7 +6,7 @@ let userValue = Number(
   prompt("Вычислит сумму чисел от 0 до указанного параметра. Введите число")
 );
 // Option 1
- function getSum(num) {
+function getSum(num) {
   let counter = 0;
   for (let i = 1; i <= num; i++) {
     counter += i;
@@ -30,14 +30,14 @@ console.log(getSum(userValue));
 процентная ставка в год — 17%,
 количество лет — 5.
 Мы пишем функцию для хорошего банка, поэтому сумма кредита не
-увеличивается. 
+увеличивается. */
 
 let creditSum = Number(prompt("Введите сумму которая Вам необходима"));
 const ANNUAL_INTEREST_RATE = 17;
 const YEARS_COUNT = 5;
 const overpayment = (sum) => ((sum * ANNUAL_INTEREST_RATE) / 100) * YEARS_COUNT;
 alert(`Переплата по кредиту будет ${overpayment(creditSum)} рублей`);
- */
+
 /* Задача 3.
 Написать функцию trimString , которая в качестве аргумента принимает три
 параметра:
@@ -45,7 +45,7 @@ alert(`Переплата по кредиту будет ${overpayment(creditSum
 значение «от»
 значение «по»
 После вызова функция должна вернуть переданную строку, обрезанную по
-значениям «от» и «по».
+значениям «от» и «по». */
 
 const userStr = prompt("Введите слово и мы его укоротим)");
 const startNumber = Number(prompt("Введите значение «от»"));
@@ -59,25 +59,24 @@ const trimString = (str, start, end) => {
   }
 };
 trimString(userStr, startNumber, endNumber);
- */
+
 /* Задача 4.
 Написать функцию getSumNumbers . Она будет принимать число и вычислять
 сумму цифр, из которых состоит число. Для 2021 это будет 5. */
 
-/* const getSumNumbers = (num) =>
+const getSumNumbers = (num) =>
   num
     .toString()
     .split("")
     .reduce((acc, item) => acc + Number(item), 0);
 console.log(getSumNumbers(2023));
 
- */
 /* Задача 5.
 Написать функцию getSum , которая принимает два целых числа a и b, они могут
 быть положительными или отрицательными, найти сумму всех чисел между
 ними, включая их, и вернуть ее. Если два числа равны, верните a или b. */
 // Option 1
-/* function getSum(a, b) {
+function getSum(a, b) {
   let count = 0;
   if (a < b) {
     for (let i = a; i <= b; i++) {
@@ -106,7 +105,7 @@ console.log(getSum(0, 1)); // == 1  0 + 1 = 1
 console.log(getSum(1, 1)); // == 1  1 Since both are same
 console.log(getSum(-1, 0)); // == -1  -1 + 0 = -1
 console.log(getSum(-1, 2)); // = 2  -1 + 0 + 1 + 2 = 2
- */
+
 /* Задача 6.
 Напишите функцию fooBoo которая принимает в качестве аргумента три
 параметра:
@@ -115,19 +114,19 @@ console.log(getSum(-1, 2)); // = 2  -1 + 0 + 1 + 2 = 2
 функцию boo которая выводит в консоль свое имя
 Если переданное булевое значение true , то запускаем функцию foo, иначе boo */
 
-/* const foo = () => console.log("foo");
+const foo = () => console.log("foo");
 const boo = () => console.log("boo");
 const fooBoo = (isBoolean, nameFoo, nameBoo) =>
   isBoolean ? nameFoo() : nameBoo();
 fooBoo(true, foo, boo);
 fooBoo(false, foo, boo);
- */
+
 // Advanced level
 /* Задача 1.
 Реализуйте функцию, который принимает 3 целочисленных значения a, b, c.
 Функция должна возвращать true , если треугольник можно построить со
 сторонами заданной длины, и false в любом другом случае */
-/* function isValidTriangle(a, b, c) {
+function isValidTriangle(a, b, c) {
   const arr = [a, b, c].sort((a, b) => a - b);
   if ((a > 0) & (b > 0) & (c > 0)) {
     return arr[0] + arr[1] > arr[2];
@@ -137,7 +136,7 @@ fooBoo(false, foo, boo);
 }
 console.log(isValidTriangle(3, 4, 5));
 console.log(isValidTriangle(1, 1, 5));
- */
+
 /* Задача 2.
 Ваша задача - разбить плитку шоколада заданного размера n x m на
 маленькие квадраты. Каждый квадрат имеет размер 1x1 и не может быть
@@ -150,10 +149,10 @@ console.log(isValidTriangle(1, 1, 5));
 надломы не требуются, если у нас нет шоколада для разделения). Ввод всегда
 будет неотрицательным целым числом.
  */
-/* function minBreaksCount(n, m) {
+function minBreaksCount(n, m) {
   return n > 0 && m > 0 ? (n - 1) * m : 0;
 }
-console.log(minBreaksCount(5, 1)); */
+console.log(minBreaksCount(5, 1));
 /* Задача 3.
 Напишите программу для вычисления общей стоимости покупки телефонов.
 Вы будете продолжать покупать телефоны (подсказка: циклы!), пока у вас не
