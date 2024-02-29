39a204760deebbf86f284a5e3bbd214693e6239c
Victor <victoralexkuznecov@gmail.com>
2024-02-29 10:19:38 +0300

    task 2

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ const user = {
 };
 delete user.name;
 delete user.email;
+
+/* Задача 2.
+Создать любой объект с двумя ключами и любыми значениями в них, а затем
+проверить есть ли в объекте определенный ключ и если есть вывести в
+консоль true  */
+
+const countries = {
+  country: "Latvia",
+  capital: "Riga",
+  isKeys: function (elem) {
+    for (const item in this) {
+      if (item === elem) {
+        return true;
+      }
+    }
+    return false;
+  },
+};
+console.log(countries.isKeys("country"));
