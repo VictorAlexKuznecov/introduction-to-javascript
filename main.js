0ebf17d3d616c7951d71447f320785c327cd9758
Victor <victoralexkuznecov@gmail.com>
2024-03-02 17:11:55 +0300

    task 2

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ const colors = ['red', 'green', 'blue']
 
 const colors = ["red", "green", "blue"];
 console.log(colors.length);
+
+/* Задача 2.
+Дан массив:
+const animals = ['monkey', 'dog', 'cat']
+Выведите в консоль его последний элемент вне зависимости от его длинны. */
+
+const animals = ["monkey", "dog", "cat"];
+console.log(animals[animals.length - 1]);
