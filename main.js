d6dcff3cf2ea38b580ff22db4802b8eac4595fcf
Victor <victoralexkuznecov@gmail.com>
2024-03-04 07:42:54 +0300

    task 2

--- main.js
+++ main.js
@@ -3,12 +3,32 @@
 const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
 Реализуйте решение двумя способами, используя function declaration & arrow
 function */
-const fibonacci = [
+/* const fibonacci = [
   0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987,
 ];
 function getFibonacci(arr) {
   arr.forEach((item) => console.log(item));
 }
+// arrow function
 getFibonacci(fibonacci);
 const getFibonacci = (arr) => arr.forEach((item) => console.log(item));
 getFibonacci(fibonacci);
+ */
+
+/* Задача 2.
+Используя метод map создайте новый массив, на основе массива users , в
+котором каждый элемент массива будет содержать строку вида:
+['member 1: Darya', 'member 2: Masha', ... etc] .
+const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton']
+Реализуйте решение двумя способами, используя function declaration & arrow
+function. */
+const users = ["Darya", "Masha", "Denis", "Vitaliy", "Polina", "Anton"];
+
+/* function formatMembers(arr) {
+  return arr.map((item, index) => `member ${index + 1}: ${item}`);
+}
+console.log(formatMembers(users)); */
+// arrow function
+const formatMembers = (arr) =>
+  arr.map((item, index) => `member ${index + 1}: ${item}`);
+console.log(formatMembers(users));
